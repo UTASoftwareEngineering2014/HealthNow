d652cc47f3e5e06f545c8094e92fbf62ca7282b8
Victor Diaz <vd31192@gmail.com>
2014-03-07 22:20:13 -0600

    Incremental commit for main activity

--- src/com/application/healthnow/MainActivity.java
+++ src/com/application/healthnow/MainActivity.java
@@ -1,15 +1,103 @@
 package com.application.healthnow;
 
+import java.util.ArrayList;
+
+import com.application.healthnow.adapter.NavDrawerListAdapter;
+import com.application.healthnow.model.NavDrawerItem;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.TypedArray;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
+	// nav drawer title
+	private CharSequence mDrawerTitle;
+	
+	// used to store app title 
+	private CharSequence mTitle;
+	
+	// slide menu items
+	private String[] navMenuTitles;
+	private TypedArray navMenuIcons;
+	
+	private ArrayList<NavDrawerItem> navDrawerItems;
+	private NavDrawerListAdapter adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mTitle = mDrawerTitle = getTitle();
+		
+		// load slide menu items
+		navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+		
+		// navigation drawer icons from resources
+		navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+		
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+		
+		navDrawerItems = new ArrayList<NavDrawerItem>();
+		
+		// adding navigation drawer items to array 
+		// Home
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+        // Find People
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+        // Photos
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+        // Communities, Will add a counter here
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
+        
+        // Recycle the typed array 
+        navMenuIcons.recycle();
+        
+        // setting the navigation drawer list adapter
+        adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
+        mDrawerList.setAdapter(adapter);
+        
+        // enabling the action bar app icon and making it behave as a toggle button
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        
+        mDrawerToggle = new ActionBarDrawerToggle(this, 
+        		mDrawerLayout, 
+        		R.drawable.ic_drawer, // navigation menu toggle icon
+        		R.string.app_name, // navigation drawer open - description accessibility
+        		R.string.app_name // navigation drawer close - description accessibility
+        		){
+        	public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				// calling onPrepareOptionsMenu() to show action bar icons
+				invalidateOptionsMenu();
+			}
+        	
+        	public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				// calling onPrepareOptionsMenu() to hide action bar icons
+				invalidateOptionsMenu();
+			}
+        };
+        
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+//        if(savedInstanceState == null)
+//        {
+//        	// the first time display the view for the first navigation item
+//        	displayView(0);
+//        }
 	}
 
 	@Override
