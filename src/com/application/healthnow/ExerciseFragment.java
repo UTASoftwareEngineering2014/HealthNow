2337b73f2b61819d3e83677a2d2848b1ebcec5f0
Victor Diaz <vd31192@gmail.com>
2014-03-08 12:28:08 -0600

    The side thing is working and responds to click on each icon.

--- src/com/application/healthnow/ExerciseFragment.java
+++ src/com/application/healthnow/ExerciseFragment.java
@@ -1,12 +1,12 @@
 package com.application.healthnow;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ExerciseFragment extends Fragment{
+public class ExerciseFragment extends Fragment {
 	
 	public ExerciseFragment() { }
 	
